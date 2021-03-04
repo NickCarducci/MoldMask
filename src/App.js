b68ab7bb3b5dfe825fc5c8e1742d029668e408cf
Nick Carducci <nmcarducci@gmail.com>
2021-03-04 13:45:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ export default function App() {
           fontFamily: '"Pacifico", sans-serif'
         }}
       >
-        moldmask
+        <a href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms.">
+          moldmask
+        </a>
       </h1>
     </div>
   );
