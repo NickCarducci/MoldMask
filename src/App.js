efb0b68d3d0d0b37174a7ed6b00422140dd96c3c
Nick <nmcarducci@gmail.com>
2021-03-04 13:31:03 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "rgb(218, 90, 197)",
+          fontFamily: '"Pacifico", sans-serif'
+        }}
+      >
+        moldmask
+      </h1>
+    </div>
+  );
+}
