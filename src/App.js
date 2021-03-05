9c589dbd3d4d89f62a8743f3cbb044011928ec80
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 08:31:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1
-        style={{
-          color: "rgb(218, 90, 197)",
-          fontFamily: '"Pacifico", sans-serif'
-        }}
-      >
-        <a href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms.">
+      <h1>
+        <a 
+          style={{
+            color: "rgb(178, 90, 197)",
+            fontFamily: '"Pacifico", sans-serif'
+          }} 
+          href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms.">
           moldmask
         </a>
       </h1>
