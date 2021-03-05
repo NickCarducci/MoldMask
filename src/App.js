0d627b016e6aa03b7b07240fa297c1b97b654f1f
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 17:11:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,16 @@ export default function App() {
       <br />
       If you'll love the death caused by masks, you'll love our other product
       line&nbsp;<a href="https://humanharvest.info">HumanHarvest</a>
-      <br />(
-      <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">B.1.1.7</a>
-      &nbsp;is something to watch) or any&nbsp;
-      <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
-        new variables after March 2021
-      </a>
+      <br />
+      <div style={{ display: "inline-block" }}>
+        <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">
+          (B.1.1.7
+        </a>
+        &nbsp;is something to watch) or any&nbsp;
+        <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
+          new variables after March 2021
+        </a>
+      </div>
     </div>
   );
 }
