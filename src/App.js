882e86a2cb1b5b8df7cc5cd571fb343b44a5d406
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 11:28:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>
-        <a 
+        <a
           style={{
             color: "rgb(178, 90, 197)",
             fontFamily: '"Pacifico", sans-serif'
-          }} 
-          href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms.">
+          }}
+          href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
+        >
           moldmask
         </a>
       </h1>
+      <br />
+      If you'll love the death caused by masks, you'll love our other product
+      line&nbsp;<a href="https://humanharvest.info">HumanHarvest</a>
     </div>
   );
 }
