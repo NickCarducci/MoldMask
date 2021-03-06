257bda8c6fcffdeb6c3afe39e89133dc56bb30d0
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 21:10:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,70 +3,84 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        <a
-          style={{
-            color: "rgb(178, 90, 197)",
-            fontFamily: '"Pacifico", sans-serif'
-          }}
-          href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
-        >
-          moldmask
-        </a>
-      </h1>
-      <span
-        style={{
-          fontSize: "8px",
-          color: "rgb(20,100,255)",
-          transform: "translateY(-5px)"
-        }}
-      >
-        ween off of oxygen with fungi!
-      </span>
-      <br />
-      If you'll love the death caused by masks, you'll love our other product
-      line&nbsp;<a href="https://humanharvest.info">HumanHarvest</a>
-      <br />
-      <div style={{ display: "inline-block" }}>
-        <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">
-          (B.1.1.7
-        </a>
-        &nbsp;is something to watch) or any&nbsp;
-        <a href="https://saverparty.xyz">new variables after March 2021</a>
-      </div>
-      <br />
-      Thanks, Wolfe Blitzer for sharing studies as fact without a control
-      so&nbsp;
-      <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
-        you can rely on weather systems to sell these things
-      </a>
       <div
         style={{
-          backgroundColor: "white",
-          width: "50%",
-          display: "flex",
-          position: "fixed",
-          bottom: "0px",
-          right: "0px"
+          textAlign: "center",
+          width: "100%",
+          maxWidth: "600px"
         }}
       >
-        <div style={{ margin: "10px", textAlign: "right" }}>
-          Have a real hankering for raising hell for you and your fellows? Check
-          out our always-pivoting
-          <br />
-          <a href="https://foiegras.life">FoieGras</a>
+        <h1>
+          <a
+            style={{
+              color: "rgb(178, 90, 197)",
+              fontFamily: '"Pacifico", sans-serif'
+            }}
+            href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
+          >
+            moldmask
+          </a>
+        </h1>
+        <span
+          style={{
+            position: "absolute",
+            fontSize: "8px",
+            color: "rgb(20,100,255)",
+            transform: "translate(-50%,-5px)"
+          }}
+        >
+          ween off of oxygen with fungi!
+        </span>
+        <br />
+        If you'll love the death caused by masks, you'll love our other product
+        line&nbsp;<a href="https://humanharvest.info">HumanHarvest</a>
+        <br />
+        <br />
+        <div style={{ display: "inline-block" }}>
+          <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">
+            (B.1.1.7
+          </a>
+          &nbsp;is something to watch) or any&nbsp;
+          <a href="https://saverparty.xyz">new variables after March 2021</a>
+        </div>
+        <br />
+        <br />
+        <div style={{ display: "inline-block" }}>
+          Thanks, Wolfe Blitzer for sharing studies as fact without a control so
+          you can rely on&nbsp;
+          <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
+            weather systems
+          </a>{" "}
+          to sell these things
         </div>
         <div
           style={{
-            border: "2px solid black",
-            width: "max-content",
-            position: "relative",
-            fontSize: "10px"
+            backgroundColor: "white",
+            width: "50%",
+            display: "flex",
+            position: "fixed",
+            bottom: "0px",
+            right: "0px"
           }}
         >
-          TRIGGER WARNING:
-          <br />
-          animal abuse; farming:
+          <div style={{ margin: "10px", textAlign: "right" }}>
+            Have a real hankering for raising hell for you and your fellows?
+            Check out our always-pivoting
+            <br />
+            <a href="https://foiegras.life">FoieGras</a>
+          </div>
+          <div
+            style={{
+              border: "2px solid black",
+              width: "max-content",
+              position: "relative",
+              fontSize: "10px"
+            }}
+          >
+            TRIGGER WARNING:
+            <br />
+            animal abuse; farming:
+          </div>
         </div>
       </div>
     </div>
