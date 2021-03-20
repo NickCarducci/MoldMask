30b309f977ec56a0f9c8383b9a49e75e96d12893
Nick Carducci <nmcarducci@gmail.com>
2021-03-19 21:20:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,167 +2,13 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <div
-        style={{
-          position: "fixed",
-          top: "0px",
-          left: "0px",
-          backgroundColor: "rgb(200, 230, 255)"
-        }}
-      >
-        <a style={{ color: "blue" }} href="https://billbiden.org">
-          BillBiden
-        </a>: does it seep into the pores?
-      </div>
-      <div
-        style={{
-          textAlign: "center",
-          width: "100%",
-          maxWidth: "600px"
-        }}
-      >
-        You cannot force, ban or move people to do something not on an
-        individual basis; you're of the choice to go visit someplace or not, and
-        territories can disclude and not be a societal-free-rider if there are
-        visible symptoms, or by a non-invasive temperature check
-        <br />
-        <br />
-        "No benefit of monoclonal antibodies after visiting a hospital nest,
-        have to make your own with mRNA without using your own punches,&nbsp;
-        <a href="https://www.nature.com/articles/cddis2016148">
-          need to guide it
-        </a>
-        "
-        <br />
-        <br />
-        "Measels do not vary as much as covid with time"
-        <br />
-        <br />
-        "Starting at a plateau, at a high level, then you blast off" - Dr. Fauci
-        <br />
-        <br />
-        The&nbsp;
-        <a href="https://www6.slac.stanford.edu/news/2020-12-18-scientists-get-most-realistic-view-yet-coronavirus-spike-protein-structure.aspx">
-          Virus
-        </a>
-        &nbsp;is an endangered species,&nbsp;
-        <a href="https://www.memorialhealthcare.org/whats-the-difference-between-covid-19-rapid-and-prc-tests/">
-          our diagnosis
-        </a>
-        &nbsp;is a&nbsp;
-        <a href="https://www.nih.gov/news-events/nih-research-matters/immune-cells-common-cold-may-recognize-sars-cov-2">
-          reaction to cold virus too
-        </a>
-        <br />
-        <h1>
-          <a
-            style={{
-              color: "rgb(178, 90, 197)",
-              fontFamily: '"Pacifico", sans-serif'
-            }}
-            href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
-          >
-            moldmask
-          </a>
-        </h1>
-        <span
-          style={{
-            position: "absolute",
-            fontSize: "8px",
-            color: "rgb(20,100,255)",
-            transform: "translate(-50%,-5px)"
-          }}
-        >
-          ween off of oxygen with fungi, grow your own&nbsp;
-          <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">
-            (B.1.1.7
-          </a>
-          !
-        </span>
-        <br />
-        If you'll love the&nbsp;
-        <a href="https://en.wikipedia.org/wiki/Joan_Hamburg">death</a>
-        &nbsp;caused by masks, you'll love our other product line&nbsp;
-        <a href="https://humanharvest.info">HumanHarvest</a>
-        <br />
-        <br />
-        <div style={{ display: "inline-block" }}>
-          Thanks, Wolfe Blitzer for sharing studies as fact without a control so
-          us mask-sellers ignore weather systems and proof that masks&nbsp;
-          <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
-            do not save lives absolutely
-          </a>
-          &nbsp; to sell these things, with only correlations that they do
-          (which can be because of age of population or intentional poisoning)
-        </div>
-        <br />
-        <br />
-        <div
-          style={{
-            padding: "6px 10px",
-            borderRadius: "8px",
-            backgroundColor: "rgb(50,50,50)",
-            color: "rgb(200,200,200)"
-          }}
-        >
-          <i
-            style={{
-              fontFamily: '"Pacifico", sans-serif'
-            }}
-          >
-            SanityCheck
-          </i>
-          <br />
-          Epidemiologists respond with a null finding. Physicists respond with
-          a&nbsp;
-          <a
-            style={{ color: "rgb(150,220,255)" }}
-            href="https://pubmed.ncbi.nlm.nih.gov/17542834/"
-          >
-            positive
-          </a>
-          &nbsp;finding (heat rises/earth’s electrons pull, only&nbsp;
-          <a
-            style={{ color: "rgb(150,220,255)" }}
-            href="https://www.stingraypartswasher.com/filtration-relative-particle-sizes-in-microns.html"
-          >
-            oxygen
-          </a>
-          &nbsp;exists in Brownian motion). Politifact’s epidemiologist consults
-          say virus’ require hosts’&nbsp;
-          <a
-            style={{ color: "rgb(150,220,255)" }}
-            href="https://www.politifact.com/factchecks/2020/jun/15/facebook-posts/claim-n95-masks-cant-stop-covid-19-particles-due-s/"
-          >
-            spittle/oils
-          </a>
-          &nbsp;and gestation time
-        </div>
-        <br />
-        <a href="https://thecritic.co.uk/face-masks-make-you-stupid/">OBEY</a>
-        &nbsp;(to say lockdown are required now that someone has the PCR for
-        cold is laughable. The root cause of hostility towards Asians is
-        lockdowns and the Duke U hostility towards lockdown orders is not cases
-        of Poly-Chain Reaction to cold, it is because you do not
-        understand&nbsp;
-        <a href="https://en.wikipedia.org/wiki/Atmospheric_circulation">
-          weather systems
-        </a>
-        )
-        <br />
-        <br />
-        If the&nbsp;
-        <a href="https://">
-          financial-canibalism, rentier-colonialism & Bernie Sanders
-        </a>
-        &nbsp;didn't exist to&nbsp;
-        <a href="https://froth.app/debt">sell us out</a>,&nbsp;the rich media
-        deductible-philantropists would want to&nbsp;
-        <a href="https://www.the-scientist.com/news-opinion/nih-cancels-funding-for-bat-coronavirus-research-project-67486">
-          kill us
-        </a>
-      </div>
+    <div
+      style={{
+        width: "100%",
+        height: "min-content",
+        backgroundColor: "rgb(200, 230, 255)"
+      }}
+    >
       <div
         style={{
           backgroundColor: "white",
@@ -192,6 +38,167 @@ export default function App() {
           animal abuse; farming:
         </div>
       </div>
+      <div
+        style={{
+          position: "fixed",
+          top: "0px",
+          left: "0px",
+          backgroundColor: "rgb(200, 230, 255)"
+        }}
+      >
+        <a style={{ color: "blue" }} href="https://billbiden.org">
+          BillBiden
+        </a>
+        : does it seep into the pores?
+      </div>
+      <br />
+      You cannot force, ban or move people to do something not on an individual
+      basis; you're of the choice to go visit someplace or not, and territories
+      can disclude and not be a societal-free-rider if there are visible
+      symptoms, or by a non-invasive temperature check
+      <br />
+      <br />
+      "No benefit of monoclonal antibodies after visiting a hospital nest, have
+      to make your own with mRNA without using your own punches,&nbsp;
+      <a href="https://www.nature.com/articles/cddis2016148">
+        need to guide it
+      </a>
+      "
+      <br />
+      <br />
+      "Measels do not vary as much as covid with time"
+      <br />
+      <br />
+      "Starting at a plateau, at a high level, then you blast off" - Dr. Fauci
+      <br />
+      <br />
+      The&nbsp;
+      <a href="https://www6.slac.stanford.edu/news/2020-12-18-scientists-get-most-realistic-view-yet-coronavirus-spike-protein-structure.aspx">
+        Virus
+      </a>
+      &nbsp;is an endangered species,&nbsp;
+      <a href="https://www.memorialhealthcare.org/whats-the-difference-between-covid-19-rapid-and-prc-tests/">
+        our diagnosis
+      </a>
+      &nbsp;is a&nbsp;
+      <a href="https://www.nih.gov/news-events/nih-research-matters/immune-cells-common-cold-may-recognize-sars-cov-2">
+        reaction to cold virus too
+      </a>
+      <br />
+      <h1>
+        <a
+          style={{
+            color: "rgb(178, 90, 197)",
+            fontFamily: '"Pacifico", sans-serif'
+          }}
+          href="https://irp.nih.gov/blog/post/2020/09/mold-exposure-sets-stage-for-severe-flu#:~:text=New%20IRP%20research%20has%20found,dramatically%20increasing%20the%20illness's%20severity.&text=In%20order%20to%20reproduce%2C%20the,by%20humans%20and%20other%20organisms."
+        >
+          moldmask
+        </a>
+      </h1>
+      <span
+        style={{
+          position: "absolute",
+          fontSize: "8px",
+          color: "rgb(20,100,255)",
+          transform: "translate(-50%,-5px)"
+        }}
+      >
+        ween off of oxygen with fungi, grow your own&nbsp;
+        <a href="https://www.cdc.gov/mmwr/volumes/70/wr/mm7003e2.htm">
+          (B.1.1.7
+        </a>
+        !
+      </span>
+      <br />
+      If you'll love the&nbsp;
+      <a href="https://en.wikipedia.org/wiki/Joan_Hamburg">death</a>
+      &nbsp;caused by masks, you'll love our other product line&nbsp;
+      <a href="https://humanharvest.info">HumanHarvest</a>
+      <br />
+      <br />
+      <div style={{ display: "inline-block" }}>
+        Thanks, Wolfe Blitzer for sharing studies as fact without a control so
+        us mask-sellers ignore weather systems and proof that masks&nbsp;
+        <a href="https://pubmed.ncbi.nlm.nih.gov/17542834/">
+          do not save lives absolutely
+        </a>
+        &nbsp; to sell these things, with only correlations that they do (which
+        can be because of age of population or intentional poisoning)
+      </div>
+      <br />
+      <br />
+      <div
+        style={{
+          padding: "6px 10px",
+          borderRadius: "8px",
+          backgroundColor: "rgb(50,50,50)",
+          color: "rgb(200,200,200)"
+        }}
+      >
+        <i
+          style={{
+            fontFamily: '"Pacifico", sans-serif'
+          }}
+        >
+          SanityCheck
+        </i>
+        <br />
+        Epidemiologists respond with a null finding. Physicists respond with
+        a&nbsp;
+        <a
+          style={{ color: "rgb(150,220,255)" }}
+          href="https://pubmed.ncbi.nlm.nih.gov/17542834/"
+        >
+          positive
+        </a>
+        &nbsp;finding (heat rises/earth’s electrons pull, only&nbsp;
+        <a
+          style={{ color: "rgb(150,220,255)" }}
+          href="https://www.stingraypartswasher.com/filtration-relative-particle-sizes-in-microns.html"
+        >
+          oxygen
+        </a>
+        &nbsp;exists in Brownian motion). Politifact’s epidemiologist consults
+        say virus’ require hosts’&nbsp;
+        <a
+          style={{ color: "rgb(150,220,255)" }}
+          href="https://www.politifact.com/factchecks/2020/jun/15/facebook-posts/claim-n95-masks-cant-stop-covid-19-particles-due-s/"
+        >
+          spittle/oils
+        </a>
+        &nbsp;and gestation time
+      </div>
+      <br />
+      <a href="https://thecritic.co.uk/face-masks-make-you-stupid/">OBEY</a>
+      &nbsp;(to say lockdown are required now that someone has the PCR for cold
+      is laughable. The root cause of hostility towards Asians is lockdowns and
+      the Duke U hostility towards lockdown orders is not cases of Poly-Chain
+      Reaction to cold, it is because you do not understand&nbsp;
+      <a href="https://en.wikipedia.org/wiki/Atmospheric_circulation">
+        weather systems
+      </a>
+      )
+      <br />
+      <br />
+      If the&nbsp;
+      <a href="https://">
+        financial-canibalism, rentier-colonialism & Bernie Sanders
+      </a>
+      &nbsp;didn't exist to&nbsp;
+      <a href="https://froth.app/debt">sell us out</a>,&nbsp;the rich media
+      deductible-philantropists would want to&nbsp;
+      <a href="https://www.the-scientist.com/news-opinion/nih-cancels-funding-for-bat-coronavirus-research-project-67486">
+        kill us
+      </a>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
